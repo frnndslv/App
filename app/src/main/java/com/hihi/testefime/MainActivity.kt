70b7d902d455cdd208c6b9c1d4bbaf6c0492b7ec
autologon <fernandinhacosta.97@hotmail.com>
2024-09-23 11:32:17 -0300

    recicleview

--- app/src/main/java/com/hihi/testefime/MainActivity.kt
+++ app/src/main/java/com/hihi/testefime/MainActivity.kt
@@ -8,11 +8,12 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var gudinhoFilmes : TextView
     lateinit var bt_verfimes : Button
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +24,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         inicializar_elementos()
+
+
 
         bt_verfimes.setOnClickListener{
             val intent = Intent(
